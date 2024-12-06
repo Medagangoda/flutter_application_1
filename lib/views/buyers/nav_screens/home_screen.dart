6c2fbbd26300324914da70e3912055bef1d7b414
Medagangoda <140582352+Medagangoda@users.noreply.github.com>
2024-12-06 06:13:46 +0530

    start_building_home_screen

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -1,14 +1,28 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/buyers/nav_screens/widgets/search_input_widget.dart';
+import 'package:flutter_application_1/views/buyers/nav_screens/widgets/welcome_text_widget.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('home screen'),
+    return Padding(
+      padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top, left: 25, right: 15),
+      child: Column(
+        children: [
+          WelcomeText(),
+          SizedBox(
+            height: 10,
+          ),
+          SearchInputWidget(),
+        ],
+      ),
     );
   }
 }
+
+
